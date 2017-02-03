73078b3417a1b4c52367c4d60ab67481b71952c3
SanKlein <paklein3@gmail.com>
2017-02-03 08:39:45 -0800

    added tilt on mousemove, fixed sections fading in and out, added parallax scrolling for stars

--- js/index.js
+++ js/index.js
@@ -1,34 +1,25 @@
-var adjectives = ['Hardworking', 'Optimistic', 'Positive', 'Ambitious', 'Growing', 'Passionate', 'Persistent', 'Patient', 'Creative'];
-var nouns = ['Entrepreneur', 'Creator', 'Programmer', 'Engineer', 'Scholar', 'Artist', 'Software Developer', 'Music Lover'];
-
-var navBarConst = 300;
-
-var debounce = function(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+var adjectives = ['Hardworking', 'Optimistic', 'Positive', 'Ambitious', 'Growing', 'Passionate', 'Persistent', 'Patient', 'Creative', 'Driven'],
+    nouns = ['Creator', 'Programmer', 'Engineer', 'Scholar', 'Artist', 'Software Developer', 'Music Lover'],
+    navBarConst = 300;
 
 var getRandom = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
 var init = function() {
-  var navBar = document.querySelector('.navbar');
-  var navButtons = document.querySelector('.navbar-buttons');
-  var navBarButtons = document.querySelectorAll('.navbar-button');
-  var title = document.querySelector('.title');
-  var adjective = '';
-  var noun = '';
+  var navBar = document.querySelector('.navbar'),
+      navButtons = document.querySelector('.navbar-buttons'),
+      navBarButtons = document.querySelectorAll('.navbar-button'),
+      title = document.querySelector('.title'),
+      adjective = '',
+      noun = '',
+      sections = document.querySelectorAll('section'),
+      stars = document.querySelector('.stars'),
+      twinkling = document.querySelector('.twinkling'),
+      speed = .05,
+      homePage = document.querySelector('.home-page'),
+      info = document.querySelector('.info'),
+      projects = document.querySelectorAll('.project');
 
   var changeNavBar = function(e) {
     var scroll = window.scrollY;
@@ -36,30 +27,32 @@ var init = function() {
     if (scroll < 0) {
       navBar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
       navBar.style.height = '80px';
+      navBar.classList.remove('shadow');
       navButtons.style.height = '80px';
       navBarButtons.forEach(function(button) {
-        button.style.height = '80px';
-        button.style.lineHeight = '80px';
+        button.style.margin = '25px 23px';
         button.style.color = '#FFF';
       })
     } else if (scroll > navBarConst) {
       navBar.style.backgroundColor = 'rgba(255, 255, 255, 1)';
       navBar.style.height = '60px';
+      navBar.classList.add('shadow');
       navButtons.style.height = '60px';
       navBarButtons.forEach(function(button) {
-        button.style.height = '60px';
-        button.style.lineHeight = '60px';
+        button.style.margin = '15px 23px';
         button.style.color = '#424242';
       })
     } else {
-      var opacity = parseFloat((scroll / navBarConst).toFixed(2));
-      var height = 80 - Math.round(20 * parseFloat(scroll / navBarConst));
+      var opacity = parseFloat((scroll / navBarConst).toFixed(2)),
+          height = 80 - Math.round(20 * parseFloat(scroll / navBarConst)),
+          margin = 25 - Math.round(10 * parseFloat(scroll / navBarConst));
+
       navBar.style.backgroundColor = 'rgba(255, 255, 255, ' + opacity + ')';
       navBar.style.height = height + 'px';
+      navBar.classList.remove('shadow');
       navButtons.style.height = height + 'px';
       navBarButtons.forEach(function(button) {
-        button.style.height = height + 'px';
-        button.style.lineHeight = height + 'px';
+        button.style.margin = margin + 'px 23px';
         button.style.color = opacity > .5 ? '#424242' : '#FFF';
       })
     }
@@ -95,20 +88,79 @@ var init = function() {
   var scrollToButton = function(e, button) {
     e.preventDefault();
     var section = document.querySelector('#' + button.dataset.link);
-    scrollTo(document.body, section.offsetTop, 500);
+    scrollTo(document.body, section.offsetTop, 300);
   };
 
   var checkHighlight = function(e) {
+    var scroll = window.scrollY;
+
     navBarButtons.forEach(function(button) {
-      var section = document.querySelector('#' + button.dataset.link);
-      var highlightAt = section.offsetTop;
-      var unhighlightAt = section.offsetTop + section.offsetHeight;
-      if (window.scrollY >= highlightAt && window.scrollY < unhighlightAt) {
+      var section = document.querySelector('#' + button.dataset.link),
+          highlightAt = section.offsetTop,
+          unhighlightAt = section.offsetTop + section.offsetHeight;
+
+      if (scroll < 0 && button.dataset.link === 'home') {
+        button.classList.add('active');
+      } else if (scroll >= highlightAt && scroll < unhighlightAt) {
         button.classList.add('active');
       } else {
-        button.classList.remove('active');
+        if (button.dataset.link === 'portfolio') {
+          if (scroll < highlightAt) {
+            button.classList.remove('active');
+          }
+        } else {
+          button.classList.remove('active');
+        }
       }
     })
+  };
+
+  var checkFade = function(e) {
+    var scroll = window.scrollY;
+    sections.forEach(function(section) {
+      var sectionHeight = section.offsetHeight,
+          sectionTop = section.offsetTop;
+      var startFadeIn = sectionTop - sectionHeight;
+      var endFadeOut = startFadeIn + (sectionHeight * 2);
+
+      if (scroll < startFadeIn || scroll > endFadeOut) {
+        section.style.opacity = '0';
+      } else {
+        var diff = Math.abs(scroll - sectionTop);
+        if (diff > sectionHeight / 4) {
+          diff = diff - (sectionHeight / 4);
+          var opacity = 1 - Math.abs(parseFloat(diff / (sectionHeight / 2)).toFixed(2));
+          section.style.opacity = opacity.toString();
+        } else {
+          section.style.opacity = '1';
+        }
+      }
+    });
+  };
+
+  var scrollStars = function(e) {
+    var windowYOffset = window.pageYOffset;
+    var change = Math.round(windowYOffset * speed);
+    var backgroundPos = "50% -" + change + "px";
+
+    stars.style.backgroundPosition = backgroundPos;
+    twinkling.style.backgroundPosition = backgroundPos;
+  };
+
+  var mouseTilt = function(e) {
+    var h = Math.floor(window.innerHeight);
+    var w = Math.floor(window.innerWidth);
+    var x = e.clientX;
+    var y = e.clientY;
+
+    var rotateX = (h / 2 - y) / 40;
+    var rotateY = (w / 2 - x) / 40;
+
+    homePage.style.transform = `rotateX(${rotateX}deg) rotateY(${-rotateY}deg)`;
+    info.style.transform = `rotateX(${rotateX}deg) rotateY(${-rotateY}deg)`;
+    projects.forEach(function(project) {
+      project.style.transform = `rotateX(${rotateX}deg) rotateY(${-rotateY}deg)`;
+    });
   };
 
   window.addEventListener('scroll', changeNavBar);
@@ -120,8 +172,14 @@ var init = function() {
     button.addEventListener('click', (e) => scrollToButton(e, button));
   });
 
-  checkHightLight();
-  window.addEventListener('scroll', checkHighlight)
-}
+  checkHighlight();
+  window.addEventListener('scroll', checkHighlight);
+
+  window.addEventListener('scroll', checkFade);
+
+  window.addEventListener('scroll', scrollStars);
+
+  window.addEventListener('mousemove', mouseTilt);
+};
 
 window.onload = init();
